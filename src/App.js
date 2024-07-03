4f12178c64ab126dabaaf45d225be086068965e9
anitamje <mjekuanita@gmail.com>
2024-07-04 00:31:31 +0200

    update authentication files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 
+import Login from './components/auth/login';
+import Register from './components/auth/register';
+
+import Header from './components/header';
+import Home from './components/home';
+
+import { AuthProvider } from './context/authContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes and Route
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 style={{color: 'red'}}>
-          Learn React
-        </h1>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Header />
+        <div className="w-full h-screen flex flex-col">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,18 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
+    apiKey: "AIzaSyBgIIcYQtfG7Ewsxgdp7cQJSTzEQkaclHs",
+    authDomain: "flower-shop-fff85.firebaseapp.com",
+    projectId: "flower-shop-fff85",
+    storageBucket: "flower-shop-fff85.appspot.com",
+    messagingSenderId: "25189165483",
+    appId: "1:25189165483:web:6a3dce7f7dac5e88d64aee",
+    measurementId: "G-SLQ8Z4H5E3"
   };
-
+  
+  
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app); // Get the auth instance
+const analytics = getAnalytics(app); // Get the analytics instance
 
-
-
-export { app, auth };
+export { app, auth, analytics };
